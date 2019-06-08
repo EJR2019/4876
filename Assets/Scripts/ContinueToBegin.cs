ac309bc59f306e81fab72b4d122eec5bb6f37048
Ethan Renfroe <erenfroe2015@gmail.com>
2019-06-08 12:17:19 -0400

    Here is what I have so far for my game, 4876!

---
+++ Assets/Scripts/ContinueToBegin.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ContinueToBegin : MonoBehaviour {
+	public void Continue ()
+	{
+		SceneManager.LoadScene("Chapter1Part1");
+	}
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour {
+	public void StartGame ()
+	{
+		SceneManager.LoadScene("Intro");
+	}
+	public void ChapterSelect ()
+	{
+		SceneManager.LoadScene("ChapterSelect");
+	}
+	public void QuitGame()
+	{
+		Debug.Log("Game ended.");
+		Application.Quit ();
+	}
+}
