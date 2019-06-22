66eabbb9f760c316060492f45cb05726149a7e93
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2019-06-22 12:54:12 -0400

    Succesful work
    Lots of things were done! I'm becoming a coder!

--- Assets/Scripts/C1P1Objectives02.cs
+++ Assets/Scripts/C1P1Objectives02.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class C1P1Objectives02
-{
 
+public class C1P1Objectives02 : MonoBehaviour {
+
+    public GameObject WD;
+    public GameObject SK;
+    public GameObject WF;
     public bool wardrobe = false;
     public bool sink = false;
     public bool waffle = false;
+    public int objectives;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,16 +20,34 @@ public class C1P1Objectives02
         {
             wardrobe = true;
             Debug.Log("All dressed!");
+            objectives++;
+            print(objectives);
+            Destroy(WD);
         }
         if (collision.gameObject.tag == "Sink")
+
         {
             sink = true;
             Debug.Log("Teeth brushed!");
+            objectives++;
+            print(objectives);
+            Destroy(SK);
         }
         if (collision.gameObject.tag == "Waffle")
         {
             waffle = true;
             Debug.Log("Finished breakfast!");
+            objectives++;
+            print(objectives);
+            Destroy(WF);
+        }
+        if (collision.gameObject.tag == "Exit" && objectives == 3)
+        {
+            SceneManager.LoadScene("Chapter1Part2");
+            
+            
         }
     }
 }
+
+
