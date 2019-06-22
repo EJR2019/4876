848e411cc93c696893a0372698aec32a3c3e2fbc
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2019-06-22 12:05:39 -0400

    Update with script problem
    Fixed script

---
+++ Assets/Scripts/C1P1Objectives02.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C1P1Objectives02
+{
+
+    public bool wardrobe = false;
+    public bool sink = false;
+    public bool waffle = false;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wardrobe")
+        {
+            wardrobe = true;
+            Debug.Log("All dressed!");
+        }
+        if (collision.gameObject.tag == "Sink")
+        {
+            sink = true;
+            Debug.Log("Teeth brushed!");
+        }
+        if (collision.gameObject.tag == "Waffle")
+        {
+            waffle = true;
+            Debug.Log("Finished breakfast!");
+        }
+    }
+}
