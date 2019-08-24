191c404d82ba3990284a300ee948298279a79665
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2019-08-24 12:56:58 -0400

    Worked on C1P4
    Worked on C1P4

---
+++ Assets/Scripts/FixFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixFile : MonoBehaviour {
+
+    public Sprite FixingFile;
+
+    public Sprite FileFixed;
+
+    private void OnMouseDown()
+    {
+        FixingTheFile();
+
+        Invoke("FixingTheFile", 10f);
+        this.GetComponent<SpriteRenderer>().sprite = FileFixed;
+    }
+    public void FixingTheFile()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = FixingFile;
+    }
+}
