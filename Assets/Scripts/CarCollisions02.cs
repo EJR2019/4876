d92464da6e4bab320441a4082fb58b4021904968
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2019-10-05 12:56:19 -0400

    Finished C2P1
    Finished C2P1

---
+++ Assets/Scripts/CarCollisions02.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarCollisions02 : MonoBehaviour
+{
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "OffTheRoad")
+        {
+            Debug.Log("Collision detected");
+            SceneManager.LoadScene("Chapter2Part1");
+        }
+        if (collision.gameObject.tag == "TransitionToC2P2")
+        {
+
+            SceneManager.LoadScene("Chapter2Part2");
+
+        }
+    }
+}
