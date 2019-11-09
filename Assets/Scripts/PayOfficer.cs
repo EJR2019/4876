e91fb3df3cd9a5364275c759ab1ed16d46a8d1cc
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2019-11-09 13:01:38 -0500

    Continuing C2P2
    Continuing C2P2

---
+++ Assets/Scripts/PayOfficer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PayOfficer : MonoBehaviour {
+
+    public GameObject stopper;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Officer") {
+            Destroy(stopper);
+            Debug.Log("Ticket payed.");
+        }
+
+        
+    }
+}
