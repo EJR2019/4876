c7e8315c716caa7254dd87ad72eece4e93acaf07
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2019-12-07 12:58:38 -0500

    Finished C2P3
    Finished C2P3

---
+++ Assets/Scripts/TSSCollisons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TSSCollisons : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Asteriod")
+        {
+            Debug.Log("Collision detected");
+            SceneManager.LoadScene("Chapter2Part3");
+
+        }
+        if (collision.gameObject.tag == "Mars")
+        {
+            SceneManager.LoadScene("Chapter2Part4");
+
+        }
+    }
+}
