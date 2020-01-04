912dce29c65e26d1b8b648912372abd4a8948214
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2020-01-04 12:56:02 -0500

    Completed C2P5
    Completed C2P5

---
+++ Assets/Scripts/UseComputer2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UseComputer2 : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Computer2")
+        {
+            SceneManager.LoadScene("Chapter2Part6");
+        }
+    }
+}
