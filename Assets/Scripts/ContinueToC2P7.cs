a688125ae1c5b7a0508632e62a7fb8426fd46ec9
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2020-01-11 12:57:59 -0500

    Completed C2P6
    Completed C2P6

---
+++ Assets/Scripts/ContinueToC2P7.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueToC2P7 : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            SceneManager.LoadScene("Chapter2Part7");
+        }
+    }
+}
